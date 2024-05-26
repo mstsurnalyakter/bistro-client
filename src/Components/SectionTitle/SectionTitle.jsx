4ded6f65e3a063bbf90ea6ebb0cc2199302c2e25
mstsurnalyakter <surnalyms@gmail.com>
2024-05-26 08:10:41 +0600

    Section title added

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+const SectionTitle = ({heading,subHeading}) => {
+  return (
+    <div className='md:w-3/12 mx-auto text-center mb-8'>
+        <p className='text-yellow-500'>--- {subHeading} ---</p>
+        <h3 className='text-4xl uppercase border-y-4 py-4'>{heading}</h3>
+    </div>
+  )
+}
+
+SectionTitle.propTypes = {
+    heading:PropTypes.string.isRequired,
+   subHeading:PropTypes.string.isRequired,
+}
+
+export default SectionTitle
--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -8,12 +8,14 @@ import slide2 from '../../assets/home/slide2.jpg'
 import slide3 from '../../assets/home/slide3.jpg'
 import slide4 from '../../assets/home/slide4.jpg'
 import slide5 from '../../assets/home/slide5.jpg'
+import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 
 
 
 const Category = () => {
   return (
-    <>
+    <section>
+        <SectionTitle heading="Order Online" subHeading="From 11:00am to 10:00pm" />
       <Swiper
         slidesPerView={4}
         spaceBetween={19}
@@ -58,7 +60,7 @@ const Category = () => {
           </h3>
         </SwiperSlide>
       </Swiper>
-    </>
+    </section>
   );
 };
 
