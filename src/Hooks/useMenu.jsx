b14f2e61707974a4cc6f4763980e50c50e8424a4
mstsurnalyakter <surnalyms@gmail.com>
2024-05-27 05:34:51 +0600

    axios setup

--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import useAxiosCommon from "./useAxiosCommon";
 
 const useMenu = () => {
+
+    const axiosCommon = useAxiosCommon()
 
     const {data:menu=[],refetch,isLoading} = useQuery({
         queryKey:['menu'],
         queryFn: async () =>{
-            const {data} = await axios.get("/menu.json");
+            const {data} = await axiosCommon.get("/menu.json");
             return data;
         }
     })
